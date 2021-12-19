df24fc0d28bd8114e12e350223ac929709c883b7
AZ <awzack@gmail.com>
2021-12-18 18:46:22 -0600

    Just pure torchvision

--- frameID/transforms.py
+++ frameID/transforms.py
@@ -3,71 +3,14 @@ import os
 import torchvision.transforms as transforms
 from torchvision.io import ImageReadMode, read_image
 
-import torch
-import torch.nn as nn
-
 from torch.utils.data import Dataset
-
-class MaybeTransformation(nn.Module):
-    """Apply a transformation with a given probability."""
-    
-    def __init__(self, transform, args:dict, prob:float=1.0):
-
-        super(MaybeTransformation, self).__init__()
-        self.transform = transform
-        self.prob = prob
-        self.args = args
-
-    def forward(self, x):
-
-        if torch.rand(1) < self.prob:
-            x = self.transform(x, **self.args)
-        return x
-
-
-def random_adjust_brightness(batch:torch.Tensor, range):
-
-    n_img = batch.shape[0]
-
-    # Brightness factors must have same number of dimensions as batch of images.
-    brightness_factors = range[0] + (torch.rand([n_img, 1, 1, 1]) * (range[1] - range[0]))
-    zero_tensor = torch.zeros_like(batch)
-
-    return torch.clamp((brightness_factors * batch) + ((1 - brightness_factors) * zero_tensor), 0.0, 1.0)
-
-def random_adjust_contrast(batch:torch.Tensor, range):
-
-    n_img = batch.shape[0]
-
-    contrast_factors = range[0] + (torch.rand([n_img, 1, 1, 1]) * (range[1] - range[0]))
-    img_means = torch.mean(transforms.functional.rgb_to_grayscale(batch), dim=(-3, -2, -1), keepdim=True)
-
-    return torch.clamp(contrast_factors * batch + (1 - contrast_factors) * img_means, 0.0, 1.0)
-
-class RandomTransformation(nn.Module):
-    """Apply a set of transformations randomly to a batch."""
-
-    def __init__(self, transforms:list):
-
-        super(RandomTransformation, self).__init__()
-        self.transforms = transforms
-
-    def forward(self, x):
-
-        # Loop through transformations
-        for t in self.transforms:
-
-            x = t(x)
-
-        return x
-
 
 class ContrastiveFrameDataset(Dataset):
     """Dataset class for contrastive learning."""
 
     IMG_EXT = ('.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif', '.tiff', '.webp')
 
-    def __init__(self, path, transforms:RandomTransformation, ext=".jpg", size=None):
+    def __init__(self, path, trs:transforms.Compose, ext=".jpg", size=None):
 
         super(ContrastiveFrameDataset, self).__init__()
 
@@ -77,6 +20,7 @@ class ContrastiveFrameDataset(Dataset):
         self.ext = ext
         self.path = path
         self.read_mode = ImageReadMode.UNCHANGED
+        self.trs = trs
 
         self.file_list = self._parse_path(self.path)
 
@@ -105,7 +49,12 @@ class ContrastiveFrameDataset(Dataset):
         # We need this format for stuff.
         x = x.float() / 255
 
-        return x
+        x_tr = self.trs(x)
+
+        return {
+            "x": x,
+            "x_transformed": x_tr
+        }
 
     def __len__(self):
 
@@ -114,31 +63,27 @@ class ContrastiveFrameDataset(Dataset):
 
 if __name__ == "__main__":
 
-    from torchvision.utils import save_image
+    from torchvision.utils import save_image, make_grid
     from torch.utils.data import DataLoader
 
     p = 0.8
 
-    trs = RandomTransformation(
+    trs = transforms.Compose(
         [
-            MaybeTransformation(random_adjust_brightness, {"range": [0.5, 1.5]}, p),
-            MaybeTransformation(random_adjust_contrast, {"range": [0.5, 1.5]}, p),
+            transforms.RandomAffine(degrees=15, translate=(0.1, 0.1), scale=(1, 1.4)),
+            transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4),
+            transforms.RandomResizedCrop(size=(144, 256), scale=(0.8, 1), ratio=(1.77, 1.78))
         ]
     )
 
-    ds = ContrastiveFrameDataset("data/ravens-lions", ".jpg")
+    ds = ContrastiveFrameDataset("data/ravens-lions", trs=trs, ext=".jpg")
 
     dl = DataLoader(ds, 64)
 
     batch = next(iter(dl))
 
-    print(batch.shape)
+    print(batch)
 
-    a = trs(batch)
+    ex_a = make_grid([ds[0]["x"], ds[0]["x_transformed"], ds[0]["x_transformed"]])
 
-    print(a.shape)
-
-    save_image(a[0, :], "example_a.jpg")
-
-    b = trs(batch)
-    save_image(b[0, :], "example_b.jpg")
+    save_image(ex_a, "example_a.jpg")
